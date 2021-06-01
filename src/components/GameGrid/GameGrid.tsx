4e048ca2f90ac01c3bd160d543d04a1c981e7511
SpasticNinja <allisonbbetancourt@gmail.com>
2021-05-31 20:23:43 -0500

    ACCESSIBLITY! Keyboard controls

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -9,11 +9,49 @@ const GameGrid = () => {
   const _grid = React.createRef<HTMLDivElement>();
 
   const onAccessibleKeyDown = (e: React.KeyboardEvent) => {
-    // TODO: accessibility, allow keyboard navigation
-    // const allTiles = Array.from(_grid.current.getElementsByClassName('game-tile')) as HTMLButtonElement[];
-    // const findCurrentFocus = allTiles.find(tile => tile === document.activeElement);
+    const allTiles = Array.from(_grid.current.getElementsByClassName('game-tile')) as HTMLButtonElement[];
+    const findCurrentFocusIndex = allTiles.findIndex(tile => tile === document.activeElement);
+    const ycoord = findCurrentFocusIndex % 10;
+    // Note: all of the keydown logic is dependent on the 9 x 10 grid. 
 
-    // console.log(findCurrentFocus);
+    switch (e.key) {
+      case 'ArrowLeft': 
+        e.preventDefault();
+        if (findCurrentFocusIndex <= 10) {
+          // need to go back to column 1
+          allTiles[80 + ycoord].focus();
+        } else {
+          allTiles[findCurrentFocusIndex - 10].focus();
+        }
+        break;
+      case 'ArrowRight':
+        e.preventDefault();
+        if (findCurrentFocusIndex > 80) {
+          // need to go back to column 1
+          allTiles[ycoord].focus();
+        } else {
+          allTiles[findCurrentFocusIndex + 10].focus();
+        }
+        break;
+      case 'ArrowDown':
+        e.preventDefault();
+        if (ycoord === 9) {
+          // need to go back to column 1
+          allTiles[findCurrentFocusIndex - 9].focus();
+        } else {
+          allTiles[findCurrentFocusIndex + 1].focus();
+        }
+        break;
+      case 'ArrowUp':
+        e.preventDefault();
+        if (ycoord === 0) {
+          // need to go back to column 1
+          allTiles[findCurrentFocusIndex + 9].focus();
+        } else {
+          allTiles[findCurrentFocusIndex - 1].focus();
+        }
+        break;
+    }
   }
 
   return (
