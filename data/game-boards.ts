02a24ac4284c356d33843981d447d3f1c07d9a9a
SpasticNinja <allisonbbetancourt@gmail.com>
2021-05-31 19:51:52 -0500

    moar boardsgit add data/game-boards.ts

--- data/game-boards.ts
+++ data/game-boards.ts
@@ -1,7 +1,25 @@
 export const gameBoardsBase = [{
-  name: 'Four Deep Colors',
+  name: 'Four deep colors',
   colors: ['#c6282c', '#e9b24e', '#457527', '#03155b']
 }, {
-  name: 'Complex Personality',
-  colors: ['#3B283A', '#eadbbd', '#89c678', '#40bb94']
+  name: 'Complex personality',
+  colors: ['#594157', '#eadbbd', '#89c678', '#40bb94']
+}, {
+  name: 'All her stories',
+  colors: ['#FFB345', '#FF705E', '#FF406F', '#283A63']
+}, {
+  name: 'Love the effort',
+  colors: ['#a31a68', '#f4d3a7', '#08156b', '#02a1d1']
+}, {
+  name: 'Feast your heart',
+  colors: ['#E15D47', '#FFD46B', '#55ADFF', '#604B8A']
+}, {
+  name: 'Bind me',
+  colors: ['#7DC6C5', '#035AA6', '#F4A548', '#862A5C']
+}, {
+  name: 'Uncommon fear',
+  colors: ['#CB666A', '#D9C5A9', '#82817F', '#1D5577']
+}, {
+  name: 'So many days',
+  colors: ['#4b1e85', '#6fae9d', '#ffcf9d', '#50a9e1']
 }]
